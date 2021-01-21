0138bff4c356d27bcf1b668b697206e1ae35fbfe
KCCT-Yamashita <r118176@g.kobe-kosen.ac.jp>
2021-01-21 13:17:20 +0900

    first

---
+++ 1-odd_or_even.py
@@ -0,0 +1,10 @@
+def odd_or_even():
+    for i in range(1,6):
+        if i % 2 != 0:
+            print("{0} is odd".format(i))
+        else:
+            print("{0} is even".format(i))
+
+
+if __name__ == "__main__":
+    odd_or_even()
---
+++ 2-print.py
@@ -0,0 +1,3 @@
+print("display statement")
+print("display statement {0}".format("string"))
+print("display {0} {1} {2}".format(1,"a","abc"))
